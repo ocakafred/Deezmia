f154f835dbb34a35be06fc4b2f6d852c19fc4a17
fred ocaka <fredocaka@gmail.com>
2022-05-24 16:18:26 -0400

    editing header and footer

---
+++ resources/views/partial/footer.blade.php
@@ -0,0 +1,137 @@
+
+    <!--====== FOOTER PART START ======-->
+    
+    <footer class="footer-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="footer-top align-items-center">
+                        
+                        <div class="footer-menu">
+                            <ul>
+                              
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-6">
+                    <div class="footer-content">
+                        <h4 class="title">About Company</h4>
+                        <p>Deemzia Agro Limited is a commercially viable and 
+                            sustainable farming enterprise with emphasis on farming and  fisheries. 
+                            </p>
+                        <ul>
+                            {{-- <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                            <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                            <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+                            <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li> --}}
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-6 col-sm-6">
+                    <div class="footer-list">
+                        <h4 class="title">Qucik Links</h4>
+                        <ul>
+                            <li><a href="#"><i class="far fa-long-arrow-right"></i> Our Services</a></li>
+                            <li><a href="#"><i class="far fa-long-arrow-right"></i> About Company</a></li>
+                            <li><a href="#"><i class="far fa-long-arrow-right"></i> Latest News</a></li>
+                            <li><a href="#"><i class="far fa-long-arrow-right"></i> Meet The Team</a></li>
+                            <li><a href="#"><i class="far fa-long-arrow-right"></i> Our Product</a></li>
+                            <li><a href="#"><i class="far fa-long-arrow-right"></i> Setting & Privecy</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                    <div class="footer-list footer-list-2">
+                        <h4 class="title">Services</h4>
+                        <ul>
+                            <li><a href="#"><i class="far fa-long-arrow-right"></i> Help & Ordering</a></li>
+                            <li><a href="#"><i class="far fa-long-arrow-right"></i> Order Tracking</a></li>
+                            <li><a href="#"><i class="far fa-long-arrow-right"></i> Return & Cancelation</a></li>
+                            <li><a href="#"><i class="far fa-long-arrow-right"></i> Delivery Schedule</a></li>
+                            <li><a href="#"><i class="far fa-long-arrow-right"></i> Get a Call</a></li>
+                            <li><a href="#"><i class="far fa-long-arrow-right"></i> Online Enquiry</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="footer-info mt-30">
+                        <h3 class="title">Contact Us</h3>
+                        <ul>
+                            <li><a href="mailto:deezmiaagrofarms2021@gmail.com"><i class="fal fa-envelope-open-text"></i> deezmiaagrofarms2021@gmail.com</a></li>
+                            <li><a href="#"><i class="far fa-map-marker-alt"></i> Lujoro Village, Amuru District, Uganda</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="footer-copyright-content d-md-flex d-block align-items-center">
+                            <p>Copyright © <span>Deemzia </span> All Rights Reserved</p>
+                            <ul>
+                                {{-- <li><a href="#">Terms & Conditions</a></li>
+                                <li><a href="#">Privacy Policy</a></li>
+                                <li><a href="#">Sitemap</a></li> --}}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!--====== FOOTER PART ENDS ======-->
+     
+    <!--====== BACK TO TOP ======-->
+    <div class="back-to-top">
+        <a href="">
+            <i class="fal fa-angle-double-up"></i>
+        </a>
+    </div>
+    <!--====== BACK TO TOP ======-->
+      
+
+
+
+
+
+    <!--====== jquery js ======-->
+    <script src="assets/js/vendor/modernizr-3.6.0.min.js"></script>
+    <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+
+    <!--====== Bootstrap js ======-->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+
+    <!--====== Slick js ======-->
+    <script src="assets/js/slick.min.js"></script>
+
+    <!--====== Isotope js ======-->
+    <script src="assets/js/isotope.pkgd.min.js"></script>
+
+    <!--====== Images Loaded js ======-->
+    <script src="assets/js/imagesloaded.pkgd.min.js"></script>
+
+    <!--====== counterup js ======-->
+    <script src="assets/js/jquery.counterup.min.js"></script>
+
+    <!--====== waypoints js ======-->
+    <script src="assets/js/waypoints.min.js"></script>
+
+    <!--====== Magnific Popup js ======-->
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+
+    <!--====== Ajax Contact js ======-->
+    <script src="assets/js/ajax-contact.js"></script>
+
+    <!--====== Main js ======-->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
---
+++ resources/views/partial/header.blade.php
@@ -0,0 +1,325 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <!--====== Required meta tags ======-->
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!--====== Title ======-->
+    <title>Deezmia - Agro Import and Export Limited- Uganda</title>
+
+    <!--====== Favicon Icon ======-->
+    <link rel="shortcut icon" href="assets/images/favicon.ico" type="image/png">
+
+    <!--====== Bootstrap css ======-->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+    <!--====== Fontawesome css ======-->
+    <link rel="stylesheet" href="assets/css/all.css">
+
+    <!--====== flaticon css ======-->
+    <link rel="stylesheet" href="assets/css/flaticon.css">
+
+    <!--====== Line Icons css ======-->
+    <link rel="stylesheet" href="assets/css/LineIcons.css">
+
+    <!--====== animate css ======-->
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+
+    <!--====== Magnific Popup css ======-->
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+
+    <!--====== Slick css ======-->
+    <link rel="stylesheet" href="assets/css/slick.css">
+
+    <!--====== Default css ======-->
+    <link rel="stylesheet" href="assets/css/default.css">
+
+    <!--====== Style css ======-->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+
+</head>
+
+<body>
+
+    <!--====== PRELOADER PART START ======-->
+    
+    <div id="loading">
+        <div id="loading-center">
+            <div id="loading-center-absolute">
+                <div class="object" id="object_one"></div>
+                <div class="object" id="object_two"></div>
+                <div class="object" id="object_three"></div>
+                <div class="object" id="object_four"></div>
+            </div>
+        </div>
+    </div>
+
+    <!--====== PRELOADER PART ENDS ======-->
+
+    <!--====== SEARCH BOX PART START ======-->
+    
+    <div class="search-overlay">
+        <div class="d-table">
+            <div class="d-table-cell">
+                <div class="search-overlay-layer"></div>
+                <div class="search-overlay-layer"></div>
+                <div class="search-overlay-layer"></div>
+                
+                <div class="search-overlay-close">
+                    <span class="search-overlay-close-line"></span>
+                    <span class="search-overlay-close-line"></span>
+                </div>
+
+                <div class="search-overlay-form">
+                    <form>
+                        <input type="text" class="input-search" placeholder="Search here...">
+                        <button type="submit"><i class='far fa-search'></i></button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--====== SEARCH BOX PART ENDS ======-->
+
+    <!--====== Sidebar Modal PART START ======-->
+
+    <div class="sidebar-modal">  
+        <div class="modal right fade" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel2">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">
+                                <i class="far fa-times"></i>
+                            </span>
+                        </button>
+                        <h2 class="modal-title" id="myModalLabel2">
+                            <a href="index.html">
+                                {{-- <img src="assets/images/logo-2.png" alt="Logo"> --}}
+                                Deezmia
+                            </a>
+                        </h2>
+                    </div>
+                    <div class="modal-body">
+                        <div class="sidebar-modal-widget">
+                            <h3 class="title">About Us</h3>
+                            <p>The rationale for Deemzia Agro Limited is to be a commercially viable and sustainable farming enterprise with emphasis on farming and  fisheries. Furthermore, its medium to long term goal is to ensure that these sustainable practices are transplanted, via learnings, to the community at large and more specifically, upcoming agricultural scholars.</p>
+                        </div>
+                        <div class="sidebar-modal-widget">
+                            {{-- <h3 class="title">Additional Links</h3>
+                            <ul>
+                                <li>
+                                    <a href="log-in.html">Log In</a>
+                                </li>
+                                <li>
+                                    <a href="sign-up.html">Sign Up</a>
+                                </li>
+                                <li>
+                                    <a href="faq.html">FAQ</a>
+                                </li>
+                                <li>
+                                    <a href="#">Logout</a>
+                                </li>
+                            </ul> --}}
+                        </div>
+                        <div class="sidebar-modal-widget">
+                            <h3 class="title">Contact Info</h3>
+                            <ul class="contact-info">
+                                <li>
+                                    <i class="far fa-map-marker-alt"></i>
+                                    Address
+                                    <span>Lujoro Village, Amuru District, Uganda</span>
+           
+                                </li>
+                                <li>
+                                    <i class="far fa-envelope"></i>
+                                    Email
+                                    <span>deezmiaagrofarms2021@gmail.com</span>
+                                </li>
+                                <li>
+                                    <i class="far fa-phone"></i>
+                                    Phone
+                                    <span>+256-781-444470</span>
+                                </li>
+                            </ul>
+                        </div>
+                        {{-- <div class="sidebar-modal-widget"> --}}
+                            {{-- <h3 class="title">Connect With Us</h3> --}}
+                            {{-- <ul class="social-list">
+                                <li>
+                                    <a href="#">
+                                        <i class='fab fa-facebook-f'></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <i class='fab fa-twitter'></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <i class='fab fa-instagram'></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <i class='fab fa-google-plus-g'></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <i class='fab fa-linkedin-in'></i>
+                                    </a>
+                                </li>
+                            </ul> --}}
+                        {{-- </div> --}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--====== Sidebar Modal PART START ======-->
+
+    <!--====== HEADER PART START ======-->
+
+    <header class="header-area">
+        <div class="header-top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="header-top-content d-flex justify-content-between align-items-center">
+                            <div class="header-top-text">
+                                <p><i class="far fa-leaf"></i> Agro Import and Export Limited</p>
+                            </div>
+                            <div class="header-top-btns d-lg-flex align-items-center d-none">
+                                {{-- <ul>
+                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                                </ul> --}}
+                                <a href="#">singin <i class="far fa-plus"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="header-info">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="header-info-item">
+                            <div class="logo">
+                                <a href="">
+                                    {{-- <img src="assets/images/logo.png" alt="logo"> --}}
+                                    <h1 style="color:bisque">Deezmia</h1> 
+                                </a>
+                                
+                            </div>
+                            <div class="header-info d-lg-flex d-none">
+                                <div class="item">
+                                    <i class="fal fa-phone"></i>
+                                    <span>Phone Number</span>
+                                    <a href="tel:+12345678999">
+                                        <h5 class="title">+256-781-444470</h5>
+                                    </a>
+                                </div>
+                                <div class="item">
+                                    <i class="fal fa-envelope-open"></i>
+                                    <span>Email Address</span>
+                                    <a href="mailto:deezmiaagrofarms2021@gmail.com">
+                                        <h5 class="title">deezmiaagrofarms2021@gmail.com</h5>
+                                    </a>
+
+                                </div>
+                            </div>
+                            <div class="header-flag d-none d-sm-block">
+                                {{-- <ul class="flag-wrap">
+                                    <li class="flag-item-top">
+                                        <a href="#" class="flag-bar d-flex align-items-center">
+                                            <img src="assets/images/flag.png" alt="Image">
+                                            <span>Eng <i class="far fa-angle-down"></i></span>
+                                        </a>
+                                        <ul class="flag-item-bottom">
+                                            <li class="flag-item">
+                                                <a href="#" class="flag-link">
+                                                    <img src="assets/images/flag-1.png" alt="Image">
+                                                    United Kingdom 
+                                                </a>
+                                            </li>
+                                        
+                                            <li class="flag-item">
+                                                <a href="#" class="flag-link">
+                                                    <img src="assets/images/flag-6.png" alt="Image">
+                                                    New Zealand
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                </ul> --}}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="header-menu">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="navigation ">
+                            <nav class="navbar navbar-expand-lg">
+                                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarFive" aria-controls="navbarFive" aria-expanded="false" aria-label="Toggle navigation">
+                                    <span class="toggler-icon"></span>
+                                    <span class="toggler-icon"></span>
+                                    <span class="toggler-icon"></span>
+                                </button> <!-- navbar toggler -->
+                                <div class="collapse navbar-collapse sub-menu-bar" id="navbarFive">
+                                    <ul class="navbar-nav mr-auto">
+                                        
+                                        <li class="nav-item active">
+                                            <a class="page-scroll" href="">Home</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="page-scroll" href="about-us.html">About Us</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="page-scroll" href="#">Services</a>
+                                            <ul class="sub-menu">
+                                                <li><a href="our-project.html">Project</a></li>
+                                                <li><a href="project-details.html">Project details</a></li>
+                                                <li><a href="our-team.html">Team</a></li>
+                                                <li><a href="our-gallery.html">Gallery</a></li>
+                                            </ul>
+                                        </li>
+                                        
+                                   
+                                        <li class="nav-item">
+                                            <a class="page-scroll" href="">Contact</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="navbar-btns">
+                                    <ul>
+                                        <li><a class="search-box" href="#"><i class="fal fa-search"></i></a></li>
+                                        <li><a class="d-none d-lg-inline" href="#myModal2" data-toggle="modal"><i class="fal fa-bars"></i></a></li>
+                                    </ul>
+                                </div>
+                            </nav> <!-- navbar -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!--====== HEADER PART ENDS ======-->
